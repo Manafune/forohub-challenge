4c972af313dd721df314aacaa0930ec8ae8c0f3a
Manafune <laycanosmi@gmail.com>
2024-06-24 08:09:07 -0500

    update flyway db

--- src/main/resources/db/migration/V1__create-table-usuarios-and-perfil.sql
+++ src/main/resources/db/migration/V1__create-table-usuarios-and-perfil.sql
@@ -9,7 +9,4 @@ create table usuarios(
 create table perfil (
                     id bigint not null auto_increment,
                     nombre varchar(255) not null,
-                    usuario_id bigint not null,
-                    primary key (id),
-                    foreign key (usuario_id) references usuarios(id)
 );
---
+++ src/main/resources/db/migration/V5__create-table-respuesta.sql
@@ -0,0 +1,10 @@
+create table respuesta(
+    id bigint auto_increment,
+    mensaje text not null,
+    topico bigint not null,
+    fechaCreacion timestamp default current_timestamp,
+    autor bigint not null,
+    solucion boolean default false,
+    constraint fk_respuesta_topico_id foreign key (topico) references topico(id),
+    constraint fk_respuesta_autor_id foreign key (autor) references usuarios(id)
+)
